03ef46eb7fef92dcc9b63cb2c951ba87c7f9a39b
Josip Milan <josip.milan@aol.com>
2024-05-16 12:44:52 +0200

    signup login work

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,25 +1,40 @@
-import { Link } from "react-router-dom";
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
 // Styling
 import "./navbar.css";
 
 const Navbar = () => {
+  const navigate = useNavigate();
   const token = localStorage.getItem("token");
+
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    navigate("/login");
+  };
 
   return (
     <ul>
       <li>
         <Link to="/">Home</Link>
       </li>
-      <li>
-        <Link to="/signup">SignUp</Link>
-      </li>
-      <li>
-        <Link to="/login">Login</Link>
-      </li>
-      {token && (
-        <li>
-          <Link to="/profile">Profile</Link>
-        </li>
+      {!token ? (
+        <>
+          <li>
+            <Link to="/signup">SignUp</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+        </>
+      ) : (
+        <>
+          <li>
+            <Link to="/profile">Profile</Link>
+          </li>
+          <li>
+            <button onClick={handleLogout}>Logout</button>
+          </li>
+        </>
       )}
     </ul>
   );
