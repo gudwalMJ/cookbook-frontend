4f00c5fec8c6072da03afad61c49d63818212811
Josip Milan <josip.milan@aol.com>
2024-06-24 14:08:10 +0200

    search fixed

--- src/Components/homePage/HomePage.js
+++ src/Components/homePage/HomePage.js
@@ -4,6 +4,7 @@ import MostRecentRecipes from "../recipeCard/MostRecentRecipes";
 import InfoBoard from "../infoBoard/InfoBoard";
 import TrendingRecipes from "../recipeCard/TrendingRecipes";
 import HighestRatedRecipes from "../recipeCard/HighestRatedRecipes";
+import RecipeCard from "../recipeCard/RecipeCard";
 import "./HomePage.css";
 
 const HomePage = ({ recipes, isLoading, noResults, error }) => {
@@ -12,7 +13,18 @@ const HomePage = ({ recipes, isLoading, noResults, error }) => {
       {isLoading && <div className="spinner"></div>}
       {noResults && <div className="no-results">No results found.</div>}
       {!noResults && recipes.length > 0 && (
-        <div className="results-message">{recipes.length} recipes found.</div>
+        <>
+          <div className="results-message">{recipes.length} recipes found.</div>
+          <div className="search-results">
+            {recipes.map((recipe) => (
+              <RecipeCard
+                key={recipe._id}
+                recipe={recipe}
+                className="search-result-card"
+              />
+            ))}
+          </div>
+        </>
       )}
       {error && <div className="error">Error: {error}</div>}
 
--- src/Components/recipeCard/RecipeCard.js
+++ src/Components/recipeCard/RecipeCard.js
@@ -1,41 +1,33 @@
-// src/components/recipeCard/RecipeCard.js
 import React from "react";
-
+import { Link } from "react-router-dom";
 import "./RecipeCard.css";
 
-const StarRating = ({ rating }) => {
-  const stars = [];
-  for (let i = 1; i <= 5; i++) {
-    stars.push(
-      <span key={i} className={`star ${i <= rating ? "filled" : ""}`}>
-        &#9733;
-      </span>
-    );
-  }
-  return <div className="star-rating">{stars}</div>;
-};
-
-const RecipeCard = ({ recipe, showRating }) => {
+const RecipeCard = ({ recipe, className }) => {
   const imageUrl =
     recipe.imageUrls && recipe.imageUrls.length > 0
       ? recipe.imageUrls[0]
       : "/images/placeholder.png"; // Use a placeholder image if no image URL is available
 
   return (
-    <div className="recipe-card">
+    <Link to={`/recipes/${recipe._id}`} className={`recipe-card ${className}`}>
       <img src={imageUrl} alt={recipe.title} className="recipe-card-image" />
       <div className="recipe-info">
         <h3>{recipe.title}</h3>
-        {showRating && recipe.averageRating && (
-          <div className="recipe-rating-container">
-            <span className="recipe-rating-text">
-              ({recipe.averageRating.toFixed(1)})
-            </span>
-            <StarRating rating={recipe.averageRating} />
+        <div className="recipe-rating-container">
+          <span className="recipe-rating-text">{recipe.rating}</span>
+          <div className="star-rating">
+            {[...Array(5)].map((_, i) => (
+              <span
+                key={i}
+                className={`star ${i < recipe.rating ? "filled" : ""}`}
+              >
+                ★
+              </span>
+            ))}
           </div>
-        )}
+        </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
