ae06a38250a6b0ff8fe3263e7a20f0684d87a4a9
Josip Milan <josip.milan@aol.com>
2024-08-05 14:57:26 +0200

    additional tweaks

--- src/Components/infoBoard/InfoBoard.js
+++ src/Components/infoBoard/InfoBoard.js
@@ -5,12 +5,12 @@ import "./InfoBoard.css";
 const InfoBoard = () => {
   return (
     <div className="info-board">
-      <h2>Welcome to our food blog!</h2>
+      <h2>Welcome to Tasty Tales!</h2>
       <div className="info-content">
         <img src="/images/profiles/admin.webp" alt="Profile" />
         <p>
-          Hi! I'm Josip. Welcome to Tasty Tales, where you'll find delicious,
-          trusted recipes that will surely fill your stomach with happiness.{" "}
+          Hi! I'm Josip. Here you'll find delicious, trusted recipes that will
+          surely fill your stomach with happiness.{" "}
           <Link to="/about">Learn more...</Link>
         </p>
       </div>
