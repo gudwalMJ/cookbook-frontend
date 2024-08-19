32a4218ee715a1f64d355fef227192549e2fc5db
Josip Milan <josip.milan@aol.com>
2024-08-19 12:21:14 +0200

    Another Link fixed

--- src/Components/recipeCard/TrendingRecipes.js
+++ src/Components/recipeCard/TrendingRecipes.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import API from "../../api/api";
+import { Link } from "react-router-dom";
 import "./TrendingRecipes.css";
 
 const TrendingRecipes = () => {
@@ -26,33 +27,39 @@ const TrendingRecipes = () => {
       <h2 className="trending-section-title">Trending</h2>
       <div className="trending-recipes-grid">
         {recipes.map((recipe) => (
-          <div key={recipe._id} className="trending-recipe-card">
-            <img
-              src={recipe.imageUrls[0]}
-              alt={recipe.title}
-              className="trending-recipe-card-image"
-            />
-            <div className="trending-recipe-info">
-              <h3>{recipe.title}</h3>
-              <div className="trending-recipe-rating-container">
-                <span className="trending-recipe-rating-text">
-                  {recipe.averageRating ? recipe.averageRating.toFixed(1) : 0}
-                </span>
-                <div className="trending-star-rating">
-                  {[...Array(5)].map((_, i) => (
-                    <span
-                      key={i}
-                      className={`trending-star ${
-                        i < recipe.averageRating ? "trending-filled" : ""
-                      }`}
-                    >
-                      ★
-                    </span>
-                  ))}
+          <Link
+            to={`/recipes/${recipe._id}`}
+            key={recipe._id}
+            className="trending-recipe-card-link"
+          >
+            <div className="trending-recipe-card">
+              <img
+                src={recipe.imageUrls[0]}
+                alt={recipe.title}
+                className="trending-recipe-card-image"
+              />
+              <div className="trending-recipe-info">
+                <h3>{recipe.title}</h3>
+                <div className="trending-recipe-rating-container">
+                  <span className="trending-recipe-rating-text">
+                    {recipe.averageRating ? recipe.averageRating.toFixed(1) : 0}
+                  </span>
+                  <div className="trending-star-rating">
+                    {[...Array(5)].map((_, i) => (
+                      <span
+                        key={i}
+                        className={`trending-star ${
+                          i < recipe.averageRating ? "trending-filled" : ""
+                        }`}
+                      >
+                        ★
+                      </span>
+                    ))}
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
+          </Link>
         ))}
       </div>
     </div>
